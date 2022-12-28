9adfcfc3e9cc767d2bb46e82a817cb98d5f160df
Barny Victor <victorbarny4@gmail.com>
2022-12-28 16:23:33 +0100

    added regex to the model

--- src/model/user.ts
+++ src/model/user.ts
@@ -5,8 +5,21 @@ const userSchema = new Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid E-mail",
+      ],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [6, "Password must be at least 6 characters long"],
+    },
     profilePicture: { type: String, default: "" },
     phoneNumber: { type: String, unique: true },
   },
